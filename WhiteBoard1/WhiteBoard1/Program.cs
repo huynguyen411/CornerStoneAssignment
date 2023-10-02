3bcbe9f4021c08c1bf242a144f30035b2a74e773
Huy Nguyen <huyscopio007@gmail.com>
2023-10-02 22:05:56 +0700

    commit

---
+++ WhiteBoard1/WhiteBoard1/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace WhiteBoard1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Star> ratings = new List<Star>()
+            {
+                new Star()
+                {
+                    Id = 1,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 4
+                },
+                new Star()
+                {
+                    Id = 2,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 5
+                },
+                new Star()
+                {
+                    Id = 3,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 4
+                },
+                new Star()
+                {
+                    Id = 4,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 5
+                },
+                new Star()
+                {
+                    Id = 5,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 4
+                },
+                new Star()
+                {
+                    Id = 6,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 3
+                },
+                new Star()
+                {
+                    Id = 7,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 0
+                },
+                new Star()
+                {
+                    Id = 8,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 1
+                },
+                new Star()
+                {
+                    Id = 9,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 4
+                },
+                new Star()
+                {
+                    Id = 10,
+                    CreatedByUserEmail = "email",
+                    NumberOfStars = 5
+                },
+            };
+            Console.WriteLine(CalculateAverageRating(ratings));
+        }
+        static double CalculateAverageRating(List<Star> ratings)
+        {
+            int numberOfRatings = ratings.Count;
+            int totalRating = ratings.Sum(r => r.NumberOfStars);
+            double averageRating = 1.0 * totalRating / numberOfRatings;
+
+            return Math.Round(averageRating * 2) / 2.0;
+        }
+    }
+    public class Star
+    {
+        public int Id { get; set; }
+        public string CreatedByUserEmail { get; set; }
+        public int NumberOfStars { get; set; }
+    }
+}
---
+++ WhiteBoard2/WhiteBoard2/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace WhiteBoard2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "Emily Johnson",
+                    Salary = 900,
+                    Department = "DU1"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Liam Anderson",
+                    Salary = 1300,
+                    Department = "DU2"
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Sophia Martinez",
+                    Salary = 1120,
+                    Department = "DU3"
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name = "Noah Wilson",
+                    Salary = 700,
+                    Department = "DU4"
+                },
+                new Employee()
+                {
+                    Id = 5,
+                    Name = "Olivia Davis",
+                    Salary = 1200,
+                    Department = "DU5"
+                },
+                new Employee()
+                {
+                    Id = 6,
+                    Name = "Ethan Thompson",
+                    Salary = 1050,
+                    Department = "DU6"
+                }
+            };
+            List<string> output = GetEmployeesWithHighSalary(employees, 1000);
+            foreach(string employee in output)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+        static List<string> GetEmployeesWithHighSalary(List<Employee> employees, int threshold) 
+        {
+            List<Employee> filteredEmployees = employees.Where(x => x.Salary > threshold).OrderBy(x => x.Name).ToList();
+            List<string> returnedEmployees = filteredEmployees.Select(x => $"{x.Name} ({x.Department})").ToList();
+
+            return returnedEmployees;
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
